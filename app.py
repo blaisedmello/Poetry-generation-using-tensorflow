d9e3692c63514079886321db225bc5d22a0b5c1d
Blaise Dmello <Blaise@lovinas-MacBook-Air.local>
2024-10-11 16:18:01 -0700

    sequence padding

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
 
 sentences = [
     'I love my dog',
@@ -9,18 +10,20 @@ sentences = [
     'Do you think my dog is amazing?'
 ]
 
-test_data = [
-    'i really love my dog',
-    'my dog loves my manatee'
-]
+#test_data = [
+#    'i really love my dog',
+#    'my dog loves my manatee'
+#]
 
-tokenizer = Tokenizer(num_words = 100)
+tokenizer = Tokenizer(num_words = 100, oov_token = "<OOV>")
 tokenizer.fit_on_texts(sentences)
 word_index = tokenizer.word_index
 
 sequences = tokenizer.texts_to_sequences(sentences)
-test_seq = tokenizer.texts_to_sequences(test_data)
+#test_seq = tokenizer.texts_to_sequences(test_data)
+padded = pad_sequences(sequences, padding = 'post', maxlen = 5, truncating = 'post')
 
 print(word_index)
 print(sequences)
-print(test_seq)
+print(padded)
+#print(test_seq)
