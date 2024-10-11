19f66002a96729584f8b58cfc27ae93e2fd01771
Blaise Dmello <Blaise@lovinas-MacBook-Air.local>
2024-10-11 16:05:06 -0700

    testing tokenizer on unseen data

--- app.py
+++ app.py
@@ -4,10 +4,23 @@ from tensorflow.keras.preprocessing.text import Tokenizer
 
 sentences = [
     'I love my dog',
-    'I love my cat'
+    'I love my cat',
+    'You love my dog!',
+    'Do you think my dog is amazing?'
+]
+
+test_data = [
+    'i really love my dog',
+    'my dog loves my manatee'
 ]
 
 tokenizer = Tokenizer(num_words = 100)
 tokenizer.fit_on_texts(sentences)
 word_index = tokenizer.word_index
+
+sequences = tokenizer.texts_to_sequences(sentences)
+test_seq = tokenizer.texts_to_sequences(test_data)
+
 print(word_index)
+print(sequences)
+print(test_seq)
